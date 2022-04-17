067f1af99e85b3f623dbbc7f5e7bee97dc26a103
djambred <djambred@gmail.com>
2022-04-18 05:16:03 +0700

    first commit

--- pert6/lib/service.dart
+++
@@ -1,9 +0,0 @@
-class AppUrl {
-  static const String liveBaseURL = "https://api.djambred.my.id/student";
-  //static const String localBaseURL = "http://10.0.2.2:4000/api/v1";
-
-  static const String baseURL = liveBaseURL;
-  static const String login = baseURL;
-  static const String register = baseURL + "/registration";
-  static const String forgotPassword = baseURL + "/forgot-password";
-}
